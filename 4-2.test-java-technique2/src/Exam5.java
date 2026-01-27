52f67186ee49b037408491bcf0cfdca86e7d6ce0
hirotaka.yokoyama <hirotaka.yokoyama@rakus-partners.co.jp>
2026-01-27 09:45:00 +0900

    Java応用２

---
+++ 4-2.test-java-technique2/src/Exam5.java
@@ -0,0 +1,6 @@
+public class Exam5 {
+public static void main(String[] args) {
+    ShoppingCart cart = new ShoppingCart();
+    cart.add()
+}
+}
---
+++ 4-2.test-java-technique2/src/ShoppingCart.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class ShoppingCart extends LinkedList{
+
+    public void add(Item item){
+        
+    }
+
+    public int getTotalPrice(Item item){
+        int total = 0;
+        for()
+    }
+
+    public int getAveragePrice(){
+        return getTotalPrice() / 
+    }
+}
