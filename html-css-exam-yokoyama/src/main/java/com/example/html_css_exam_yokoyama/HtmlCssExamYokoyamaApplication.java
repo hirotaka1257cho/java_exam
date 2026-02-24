0bcf4386b3314f6f3846e67da3808a761b997a92
hirotaka.yokoyama <hirotaka.yokoyama@rakus-partners.co.jp>
2026-02-24 18:01:05 +0900

    HTMLテスト

---
+++ html-css-exam-yokoyama/src/main/java/com/example/html_css_exam_yokoyama/HtmlCssExamYokoyamaApplication.java
@@ -0,0 +1,13 @@
+package com.example.html_css_exam_yokoyama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlCssExamYokoyamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlCssExamYokoyamaApplication.class, args);
+	}
+
+}
---
+++ html-css-exam-yokoyama/src/test/java/com/example/html_css_exam_yokoyama/HtmlCssExamYokoyamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.html_css_exam_yokoyama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlCssExamYokoyamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
